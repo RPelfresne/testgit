83ebe3a94dc4390582ddf7879eb82dca66283028
RPelfresne <49885062+RPelfresne@users.noreply.github.com>
2022-06-23 10:53:06 +0200

    code initial

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+basic.showLeds(`
+    . . . # #
+    . . # # .
+    . . # . .
+    . # # . .
+    # # . . .
+    `)
 basic.forever(function () {
-	
+    basic.showIcon(IconNames.Skull)
+    basic.pause(500)
+    basic.showIcon(IconNames.Ghost)
 })
